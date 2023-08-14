5af91fffb75077fba27d307ffb42f1735c4e8d06
Sir Chester <tolani0017@gmail.com>
2023-08-14 15:27:09 -0400

    seems to work so far. learn the tau using cross validation or something.
    Dont add gradients of tau

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 K, L, d, T, dt = 100, 5, 3, 200, 1
 # Training hyperparameters
 learning_rate = 1e-3
-num_epochs = 1000
+num_epochs = 20000
 # base firing rate
 time = np.arange(0, T, dt)/100
 
 intensity, binned, spikes = sd.generate_spike_train(time, spike_type='2peaks', num_trials=K)
-#sd.plot_intensity_and_spikes(time, intensity, binned, spikes)
+# sd.plot_intensity_and_spikes(time, intensity, binned, spikes)
 
 # Manual Implementation
 Y = binned  # K x T
@@ -20,47 +20,58 @@ B = generate_bsplines(time, d)  # T x T. The coefficient (beta) will be regulari
 
 G = np.random.rand(K, L)
 beta = np.random.rand(L, T)
-
-G1 = np.copy(G)
-beta1 = np.copy(beta)
+d = np.random.rand(K)
+tausq = 0.01*np.ones(L) # 10*np.square(np.random.rand(L))
 
 G_grads = []
 beta_grads = []
-loss = []
+d_grads = []
+tausq_grads = []
+losses = []
 eps=1e-6
 for epoch in range(num_epochs):
     # Forward pass and gradient computation
-    loss, dG, dbeta = log_prob(Y, B, G1, beta1, dt)
+    loss, dd, dG, dbeta, dtausq = log_prob(Y, B, d, G, beta, tausq, dt)
 
     # # verify gradient using finite difference
-    # dG_num, dbeta_num = compute_numerical_grad(Y, B, G1, beta1, dt, eps)
+    # dd_num, dG_num, dbeta_num, dtausq_num = compute_numerical_grad(Y, B, d, G, beta, tausq, dt, eps)
+    # dd_error = np.mean(np.square(dd - dd_num))
     # dG_error = np.mean(np.square(dG - dG_num))
     # dbeta_error = np.mean(np.square(dbeta - dbeta_num))
+    # dtausq_error = np.mean(np.square(dtausq - dtausq_num))
+    # d_grads.append(dd_error)
     # G_grads.append(dG_error)
     # beta_grads.append(dbeta_error)
+    # tausq_grads.append(dtausq_error)
+    # print(f"Epoch {epoch}, dd_error {dd_error}, dG_error {dG_error}, dbeta_error {dbeta_error}, dtausq_error {dtausq_error}")
 
     # Update parameters using gradients
-    G1 -= learning_rate * dG
-    beta1 -= learning_rate * dbeta
+    d -= learning_rate * dd
+    G -= learning_rate * dG
+    beta -= learning_rate * dbeta
+    # tausq -= learning_rate * dtausq
     # Store losses and gradients
-    loss.append(loss.item())
+    losses.append(loss)
+    if epoch % 1000 == 0:
+        print(f"Epoch {epoch}, Loss {loss}")
 
-plt.plot(np.arange(0, num_epochs), loss)
+plt.plot(np.arange(0, num_epochs), losses)
 plt.show()
-# plt.plot(np.arange(0, num_epochs), G_grads)
-# plt.show()
-# plt.plot(np.arange(0, num_epochs), beta_grads)
-# plt.show()
+plt.plot(np.arange(0, num_epochs), G_grads)
+plt.show()
+plt.plot(np.arange(0, num_epochs), beta_grads)
+plt.show()
 
-lambda_manual = compute_lambda(B, G1, beta1)
+lambda_manual = compute_lambda(B, d, G, beta)
 avg_lambda_manual = np.mean(lambda_manual, axis=0)
 plt.plot(time, avg_lambda_manual)
 plt.show()
+intensity_error = np.mean(np.square(intensity - avg_lambda_manual))
 for i in range(K):
     plt.plot(time, lambda_manual[i, :])
 plt.show()
 
-latent_factors_manual = compute_latent_factors(B, beta1)
+latent_factors_manual = compute_latent_factors(B, beta)
 for i in range(L):
     plt.plot(time, latent_factors_manual[i, :])
 plt.show()
--- src/manual_implemetation.py
+++ src/manual_implemetation.py
@@ -1,47 +1,93 @@
 import numpy as np
+from numpy.linalg import det
 
-def log_prob(Y, B, G, beta, dt):
-    # Forward pass as before
-    GBetaB = np.dot(np.dot(G, beta), B)
-    lambda_del_t = np.exp(GBetaB) * dt
-    log_likelihood = np.sum(GBetaB * Y - lambda_del_t)
+
+def log_prob(Y, B, d, G, beta, beta_tausq, dt):
+    L, P = beta.shape
+    J = np.ones_like(Y)
+    diagdJ_plus_GBetaB = d[:, np.newaxis] * J + G @ beta @ B
+    lambda_del_t = np.exp(diagdJ_plus_GBetaB) * dt
+    Omega = create_precision_matrix(P)
+    BetaOmegaBeta = beta @ Omega @ beta.T
+
+    log_likelihood = np.sum(diagdJ_plus_GBetaB * Y - lambda_del_t)
+    log_prior_beta = 0.5 * (L * np.log((2 * np.pi) ** (-P) * det(Omega)) +
+                            P * np.sum(np.log(beta_tausq)) -
+                            beta_tausq.T @ np.diag(BetaOmegaBeta))
+    log_likelihood += log_prior_beta
 
     # Manual gradients
-    y_minus_lambdadt_times_B = np.dot((Y - lambda_del_t), B.T)
-    dlogL_dG = np.dot(y_minus_lambdadt_times_B, beta.T)
-    dlogL_dbeta = np.dot(G.T, y_minus_lambdadt_times_B)
+    y_minus_lambdadt = Y - lambda_del_t
+    y_minus_lambdadt_times_B = y_minus_lambdadt @ B.T
 
-    return -log_likelihood, -dlogL_dG, -dlogL_dbeta
+    dLogL_dd = np.sum(y_minus_lambdadt, axis=1)
+    dlogL_dG = y_minus_lambdadt_times_B @ beta.T
+    dlogL_dbeta = (G.T @ y_minus_lambdadt_times_B -
+                   beta_tausq[:, np.newaxis] * beta @ Omega)
+    dlogL_beta_tausq = 0.5 * (P * beta_tausq ** (-1) - np.diag(BetaOmegaBeta))
 
-def compute_lambda(B, G, beta):
-    GBetaB = np.dot(np.dot(G, beta), B)
-    lambda_ = np.exp(GBetaB)
+    return -log_likelihood, -dLogL_dd, -dlogL_dG, -dlogL_dbeta, -dlogL_beta_tausq
+
+
+def create_precision_matrix(P):
+    Omega = np.zeros((P, P))
+    # fill the main diagonal with 2s
+    np.fill_diagonal(Omega, 2)
+    # fill the subdiagonal and superdiagonal with -1s
+    np.fill_diagonal(Omega[1:], -1)
+    np.fill_diagonal(Omega[:, 1:], -1)
+    # set the last element to 1
+    Omega[-1, -1] = 1
+    return Omega
+
+
+def compute_lambda(B, d, G, beta):
+    J = np.ones((len(d), B.shape[1]))
+    diagdJ_plus_GBetaB = d[:, np.newaxis] * J + np.dot(np.dot(G, beta), B)
+    lambda_ = np.exp(diagdJ_plus_GBetaB)
     return lambda_
+
 
 def compute_latent_factors(B, beta):
     N = np.dot(beta, B)
     return N
 
-def compute_numerical_grad(Y, B, G, beta, dt, eps=1e-6):
+
+def compute_numerical_grad(Y, B, d, G, beta, beta_tausq, dt, eps=1e-6):
     """Compute numerical gradient of func w.r.t G"""
     G_grad = np.zeros_like(G)
     beta_grad = np.zeros_like(beta)
+    d_grad = np.zeros_like(d)
+    beta_tausq_grad = np.zeros_like(beta_tausq)
+    lk1, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
     for i in range(G.shape[0]):
         for j in range(G.shape[1]):
-            lk1, _, _ = log_prob(Y, B, G, beta, dt)
             orig = G[i, j]
-            G[i,j] = orig + eps
-            lk2, _, _ = log_prob(Y, B, G, beta, dt)
+            G[i, j] = orig + eps
+            lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
             G[i, j] = orig
-            G_grad[i,j] = (lk1 - lk2) / eps
+            G_grad[i, j] = (lk1 - lk2) / eps
 
     for i in range(beta.shape[0]):
         for j in range(beta.shape[1]):
-            lk1, _, _ = log_prob(Y, B, G, beta, dt)
             orig = beta[i, j]
-            beta[i,j] = orig + eps
-            lk2, _, _ = log_prob(Y, B, G, beta, dt)
+            beta[i, j] = orig + eps
+            lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
             beta[i, j] = orig
-            beta_grad[i,j] = (lk1 - lk2) / eps
+            beta_grad[i, j] = (lk1 - lk2) / eps
 
-    return -G_grad, -beta_grad
+    for i in range(d.shape[0]):
+        orig = d[i]
+        d[i] = orig + eps
+        lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+        d[i] = orig
+        d_grad[i] = (lk1 - lk2) / eps
+
+    for i in range(beta_tausq.shape[0]):
+        orig = beta_tausq[i]
+        beta_tausq[i] = orig + eps
+        lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+        beta_tausq[i] = orig
+        beta_tausq_grad[i] = (lk1 - lk2) / eps
+
+    return -d_grad, -G_grad, -beta_grad, -beta_tausq_grad
