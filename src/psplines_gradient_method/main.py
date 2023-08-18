a65fbfd670b14eab9c87a2cb4d76a86d09273eb6
Sir Chester <tolani0017@gmail.com>
2023-08-18 19:47:17 -0400

    Better simulated data for testing. works so far. Might still be an issue with basis spline tho

--- src/psplines_gradient_method/main.py
+++ src/psplines_gradient_method/main.py
@@ -4,33 +4,34 @@ from src.psplines_gradient_method.manual_implemetation import log_prob, compute_
 from src.psplines_gradient_method.generate_bsplines import generate_bsplines
 import matplotlib.pyplot as plt
 
-K, L, degree, T, dt = 100, 5, 3, 200, 1
+K, degree, T, dt = 100, 3, 200, 1
+intensity_type = ('constant', '1peak', '2peaks')
+L = len(intensity_type)
 # Training hyperparameters
 learning_rate = 1e-3
-num_epochs = 20
+num_epochs = 50000
 # base firing rate
 time = np.arange(0, T, dt)/100
 
-intensity, binned, spikes = sd.generate_spike_train(time, spike_type='2peaks', num_trials=K)
-# sd.plot_intensity_and_spikes(time, intensity, binned, spikes)
+latent_factors = sd.generate_latent_factors(time, intensity_type=intensity_type)
+intensity, binned, spikes = sd.generate_spike_trains(latent_factors, (0.1, 0.13, 0.13), (-3, -3, -3), (1/3, 1/3, 1/3), 100)
+K = binned.shape[0]
+
+sd.plot_intensity_and_spikes(time, latent_factors, intensity, binned, spikes)
 
 # Manual Implementation
 Y = binned  # K x T
 B = generate_bsplines(time, degree)  # T x T. The coefficient (beta) will be regularized
-start = 0
-num_basis = 10
-for i in range(num_basis):
-    plt.plot(time[start:(start+num_basis)], B[i+start, start:(start+num_basis)])
-plt.show()
-# check if any element of B is less than 0
-a = np.any(B < 0)
-# check which element of B is less than 0
-b = np.where(B < 0)
+# start = 0
+# num_basis = 10
+# for i in range(num_basis):
+#     plt.plot(time[start:(start+num_basis)], B[i+start, start:(start+num_basis)])
+# plt.show()
 
 G = np.random.rand(K, L)
-beta = np.random.rand(L, T)
+beta = np.random.rand(L, B.shape[0])
 d = np.random.rand(K)
-tausq = 0.01*np.ones(L) # 10*np.square(np.random.rand(L))
+tausq = 100*np.ones(L) # 10*np.square(np.random.rand(L))
 
 G_grads = []
 beta_grads = []
@@ -42,17 +43,17 @@ for epoch in range(num_epochs):
     # Forward pass and gradient computation
     loss, dd, dG, dbeta, dtausq = log_prob(Y, B, d, G, beta, tausq, dt)
 
-    # verify gradient using finite difference
-    dd_num, dG_num, dbeta_num, dtausq_num = compute_numerical_grad(Y, B, d, G, beta, tausq, dt, eps)
-    dd_error = np.mean(np.square(dd - dd_num))
-    dG_error = np.mean(np.square(dG - dG_num))
-    dbeta_error = np.mean(np.square(dbeta - dbeta_num))
-    dtausq_error = np.mean(np.square(dtausq - dtausq_num))
-    d_grads.append(dd_error)
-    G_grads.append(dG_error)
-    beta_grads.append(dbeta_error)
-    tausq_grads.append(dtausq_error)
-    print(f"Epoch {epoch}, dd_error {dd_error}, dG_error {dG_error}, dbeta_error {dbeta_error}, dtausq_error {dtausq_error}")
+    # # verify gradient using finite difference
+    # dd_num, dG_num, dbeta_num, dtausq_num = compute_numerical_grad(Y, B, d, G, beta, tausq, dt, eps)
+    # dd_error = np.mean(np.square(dd - dd_num))
+    # dG_error = np.mean(np.square(dG - dG_num))
+    # dbeta_error = np.mean(np.square(dbeta - dbeta_num))
+    # dtausq_error = np.mean(np.square(dtausq - dtausq_num))
+    # d_grads.append(dd_error)
+    # G_grads.append(dG_error)
+    # beta_grads.append(dbeta_error)
+    # tausq_grads.append(dtausq_error)
+    # print(f"Epoch {epoch}, dd_error {dd_error}, dG_error {dG_error}, dbeta_error {dbeta_error}, dtausq_error {dtausq_error}")
 
     # Update parameters using gradients
     d -= learning_rate * dd
@@ -66,24 +67,26 @@ for epoch in range(num_epochs):
 
 plt.plot(np.arange(0, num_epochs), losses)
 plt.show()
-plt.plot(np.arange(0, num_epochs), G_grads)
-plt.show()
-plt.plot(np.arange(0, num_epochs), beta_grads)
-plt.show()
 
 lambda_manual = compute_lambda(B, d, G, beta)
 avg_lambda_manual = np.mean(lambda_manual, axis=0)
 plt.plot(time, avg_lambda_manual)
 plt.show()
-np.mean(np.square(intensity - avg_lambda_manual))
+np.mean(np.square(intensity - lambda_manual))
 for i in range(K):
-    plt.plot(time, lambda_manual[i, :])
+    plt.plot(time, lambda_manual[i, :] + i*0.1)
 plt.show()
 
 latent_factors_manual = compute_latent_factors(B, beta)
 for i in range(L):
+    plt.plot(time, beta[i, 1:201])
     plt.plot(time, latent_factors_manual[i, :])
-plt.show()
+    plt.title(f'beta[{i}, :]')
+    plt.show()
+
+# for i in range(L):
+#     plt.plot(time, latent_factors_manual[i, :])
+# plt.show()
 
 
 # def horseshoe_prior(shape):
--- src/simulate_data.py
+++ src/simulate_data.py
@@ -1,58 +1,94 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_spike_train(time, spike_type='2peaks', num_trials=100):
-    dt = 0.01
 
-    if spike_type == 'constant':
-        # probability of firing is 0.2
-        y = np.zeros_like(time) + 0.2 / dt
-        intensity = y
+def generate_spike_trains(latent_factors, coeff, bias, ratio, num_trials=100):
 
-    elif spike_type == 'sine':
-        # Generate y values for sine function with period 2
-        y = np.sin(2 * np.pi * time / 2)
-        # normalize and rescale y
-        intensity = 0.45 * (y - np.min(y)) / (np.max(y) - np.min(y)) + 0.05
+    num_factors, num_timesteps = latent_factors.shape
+    if num_factors == 1:
+        ratio = [1]
+    # make sure intensity_type and ratio have the same length
+    assert num_factors == len(ratio)
+    # make sure ratio sums to 1
+    ratio = np.array(ratio)
+    ratio = ratio / np.sum(ratio)
 
-    elif spike_type == '1peak':
-        y = np.zeros_like(time)
-        y[time >= 0.25] = np.sin(2 * np.pi * (time[time >= 0.25] - 0.25) / 0.75)
-        y[time > 0.625] = 0
-        intensity = 0.45 * (y - np.min(y)) / (np.max(y) - np.min(y)) + 0.05
+    intensity = np.zeros((num_trials, num_timesteps))
+    binned = np.zeros((num_trials, num_timesteps))
+    last_binned_index = 0
+    # loop over the rows of latent_factors
+    for i in range(num_factors):
+        neuron_count = int(num_trials * ratio[i])
+        intensity[last_binned_index:(last_binned_index+neuron_count), :] = np.vstack([np.exp(bias[i] + coeff[i] * latent_factors[i, :])] * neuron_count)
+        binned[last_binned_index:(last_binned_index+neuron_count), :] = (
+            np.random.poisson(intensity[last_binned_index:(last_binned_index+neuron_count), :]))
+        last_binned_index += neuron_count
 
-    elif spike_type == '2peaks':
-        y = np.zeros_like(time)
-        y[time >= 0.25] = np.sin(2 * np.pi * (time[time >= 0.25] - 0.25) / 0.75)
-        y[time > 0.625] = 0
-        y[time >= 1.25] = np.sin(2 * np.pi * (time[time >= 1.25] - 1.25) / 0.75)
-        y[time > 1.625] = 0
-        intensity = 0.4 * (y - np.min(y)) / (np.max(y) - np.min(y)) + 0.1
-
-    binned = np.random.binomial(1, intensity, size=(num_trials, len(intensity)))
-    spikes = np.where(binned == 1)
+    # pick only the first last_binned_index rows of binned
+    binned = binned[:last_binned_index, :]
+    intensity = intensity[:last_binned_index, :]
+    spikes = np.where(binned >= 1)
 
     return intensity, binned, spikes
 
-def plot_intensity_and_spikes(t, r, b, s):
-    # t is time
-    # r is intensity/rate
-    # b is binned spikes
-    # s is spikes
-    plt.plot(t, r)
+def generate_latent_factors(time, intensity_type=('constant', '1peak', '2peaks')):
+
+    # intensity_type is a string
+    if isinstance(intensity_type, str):
+        intensity_type = [intensity_type]
+
+    num_timesteps = len(time)
+    latent_factors = np.zeros((len(intensity_type), num_timesteps))
+    for i, itype in enumerate(intensity_type):
+        rate = np.zeros_like(time)
+
+        if itype == 'constant':
+            nothing_to_do = True
+
+        elif itype == 'sine':
+            # Generate y values for sine function with period 2
+            rate = np.sin(2 * np.pi * time / 2)
+
+        elif itype == '1peak':
+            rate[time >= 0.25] = np.sin(2 * np.pi * (time[time >= 0.25] - 0.25) / 0.75)
+            rate[time > 0.625] = 0
+
+
+        elif itype == '2peaks':
+            rate[time >= 0.25] = np.sin(2 * np.pi * (time[time >= 0.25] - 0.25) / 0.75)
+            rate[time > 0.625] = 0
+            rate[time >= 1.25] = np.sin(2 * np.pi * (time[time >= 1.25] - 1.25) / 0.75)
+            rate[time > 1.625] = 0
+
+        latent_factors[i, :] = 15 * rate + 5
+
+    return latent_factors
+
+
+def plot_intensity_and_spikes(time, latent_factors, intensity, binned, spikes):
+
+    # plot latent factors
+    for i in range(latent_factors.shape[0]):
+        plt.plot(time, latent_factors[i, :] + i)
     plt.show()
 
+    # plot neuron intensities
+    for i in range(intensity.shape[0]):
+        plt.plot(time, intensity[i, :] + i*0.1)
+    plt.show()
+
+    # plot binned spikes
     _, ax = plt.subplots()
-    ax.imshow(b)
+    ax.imshow(binned)
     ax.invert_yaxis()
     plt.show()
 
     # Group entries by unique values of s[0]
-    unique_s_0 = np.unique(s[0])
+    unique_s_0 = np.unique(spikes[0])
     grouped_s = []
     for i in unique_s_0:
-        indices = np.where(s[0] == i)[0]
-        values = s[1][indices]
+        indices = np.where(spikes[0] == i)[0]
+        values = spikes[1][indices]
         grouped_s.append((i, values))
     for group in grouped_s:
         plt.scatter(group[1], np.zeros_like(group[1]) + group[0], s=1, c='black')
