d7f40d0b98a541b19b70c5fed4e4b70f37cb2f08
Sir Chester <tolani0017@gmail.com>
2023-08-26 14:13:11 -0400

    Compared the optimization of log likelihood and log priors with optimization of log likelihood and penalty
    This is specifically for the beta matrix. The penalties used were squared first derivative of beta, and squared second derivative of beta.

--- src/psplines_gradient_method/main.py
+++ src/psplines_gradient_method/main.py
@@ -1,35 +1,41 @@
 import numpy as np
 import src.simulate_data as sd
-from src.psplines_gradient_method.manual_implemetation import log_prob, compute_lambda, compute_latent_factors, compute_numerical_grad
+from src.psplines_gradient_method.manual_implemetation import log_prob, compute_lambda, compute_latent_factors, \
+    compute_numerical_grad, log_obj
 from src.psplines_gradient_method.generate_bsplines import generate_bsplines
 import matplotlib.pyplot as plt
 
-K, degree, T, dt = 100, 3, 200, 1
+K, degree, T = 100, 3, 200
 intensity_type = ('constant', '1peak', '2peaks')
 L = len(intensity_type)
 # Training hyperparameters
 learning_rate = 1e-3
 num_epochs = 50000
 # base firing rate
-time = np.arange(0, T, dt)/100
+time = np.arange(0, T, 1)/100
+dt = time[1] - time[0]
 
 latent_factors = sd.generate_latent_factors(time, intensity_type=intensity_type)
-intensity, binned, spikes = sd.generate_spike_trains(latent_factors, (0.1, 0.13, 0.13), (-3, -3, -3), (1/3, 1/3, 1/3), 100)
+np.random.seed(0)
+intensity, binned, spikes = sd.generate_spike_trains(latent_factors, (0.1, 0.13, 0.13), (-3, -3, -3), (1/3, 1/3, 1/3), K)
 K = binned.shape[0]
 
-sd.plot_intensity_and_spikes(time, latent_factors, intensity, binned, spikes)
+# sd.plot_intensity_and_spikes(time, latent_factors, intensity, binned, spikes)
 
 # Manual Implementation
 Y = binned  # K x T
 B = generate_bsplines(time, degree)  # T x T. The coefficient (beta) will be regularized
-# start = 0
+# start = 190
 # num_basis = 10
 # for i in range(num_basis):
 #     plt.plot(time[start:(start+num_basis)], B[i+start, start:(start+num_basis)])
 # plt.show()
 
+np.random.seed(0)
 G = np.random.rand(K, L)
+np.random.seed(0)
 beta = np.random.rand(L, B.shape[0])
+np.random.seed(0)
 d = np.random.rand(K)
 tausq = 100*np.ones(L) # 10*np.square(np.random.rand(L))
 
@@ -41,25 +47,43 @@ losses = []
 eps=1e-4
 for epoch in range(num_epochs):
     # Forward pass and gradient computation
-    loss, dd, dG, dbeta, dtausq = log_prob(Y, B, d, G, beta, tausq, dt)
+
+    # tausq = 2*tausq
+    # loss, dd, dG, dbeta, dtausq = log_prob(Y, B, d, G, beta, tausq, dt)
+    #
+    # # # verify gradient using finite difference
+    # # dd_num, dG_num, dbeta_num, dtausq_num = compute_numerical_grad(Y, B, d, G, beta, tausq, dt, log_prob, eps)
+    # # dd_error = np.mean(np.square(dd - dd_num))
+    # # dG_error = np.mean(np.square(dG - dG_num))
+    # # dbeta_error = np.mean(np.square(dbeta - dbeta_num))
+    # # dtausq_error = np.mean(np.square(dtausq - dtausq_num))
+    # # d_grads.append(dd_error)
+    # # G_grads.append(dG_error)
+    # # beta_grads.append(dbeta_error)
+    # # tausq_grads.append(dtausq_error)
+    # # print(f"Epoch {epoch}, dd_error {dd_error}, dG_error {dG_error}, dbeta_error {dbeta_error}, dtausq_error {dtausq_error}")
+
+    result = log_obj(Y, B, d, G, beta, tausq, dt)
+    loss, dd, dG, dbeta, lk, lp = (
+        result["loss"], result["dLogL_dd"], result["dlogL_dG"], result["dlogL_dbeta"], result["log_likelihood"], result["penalty"])
+    # epoch=0
+    # print(f"Epoch {epoch}, loss {lk}, penalty {lp}")
 
     # # verify gradient using finite difference
-    # dd_num, dG_num, dbeta_num, dtausq_num = compute_numerical_grad(Y, B, d, G, beta, tausq, dt, eps)
+    # dd_num, dG_num, dbeta_num, _ = compute_numerical_grad(Y, B, d, G, beta, tausq, dt, log_obj, eps)
     # dd_error = np.mean(np.square(dd - dd_num))
     # dG_error = np.mean(np.square(dG - dG_num))
     # dbeta_error = np.mean(np.square(dbeta - dbeta_num))
-    # dtausq_error = np.mean(np.square(dtausq - dtausq_num))
     # d_grads.append(dd_error)
     # G_grads.append(dG_error)
     # beta_grads.append(dbeta_error)
-    # tausq_grads.append(dtausq_error)
-    # print(f"Epoch {epoch}, dd_error {dd_error}, dG_error {dG_error}, dbeta_error {dbeta_error}, dtausq_error {dtausq_error}")
+    # print(f"Epoch {epoch}, dd_error {dd_error}, dG_error {dG_error}, dbeta_error {dbeta_error}")
 
     # Update parameters using gradients
     d -= learning_rate * dd
     G -= learning_rate * dG
     beta -= learning_rate * dbeta
-    tausq -= learning_rate * dtausq
+    # tausq -= learning_rate * dtausq
     # Store losses and gradients
     losses.append(loss)
     if epoch % 1000 == 0:
@@ -74,12 +98,12 @@ plt.plot(time, avg_lambda_manual)
 plt.show()
 np.mean(np.square(intensity - lambda_manual))
 for i in range(K):
-    plt.plot(time, lambda_manual[i, :] + i*0.1)
+    plt.plot(time, lambda_manual[i, :] + i*10)
 plt.show()
 
 latent_factors_manual = compute_latent_factors(B, beta)
 for i in range(L):
-    plt.plot(time, beta[i, 1:201])
+    plt.plot(np.concatenate([[time[0]-dt], time]), beta[i, :])
     plt.plot(time, latent_factors_manual[i, :])
     plt.title(f'beta[{i}, :]')
     plt.show()
--- src/psplines_gradient_method/manual_implemetation.py
+++ src/psplines_gradient_method/manual_implemetation.py
@@ -14,7 +14,7 @@ def log_prob(Y, B, d, G, beta, beta_tausq, dt):
     log_prior_beta = 0.5 * (L * np.log((2 * np.pi) ** (-P) * det(Omega)) +
                             P * np.sum(np.log(beta_tausq)) -
                             beta_tausq.T @ np.diag(BetaOmegaBeta))
-    log_likelihood += log_prior_beta
+    loss = log_likelihood + log_prior_beta
 
     # Manual gradients
     y_minus_lambdadt = Y - lambda_del_t
@@ -26,7 +26,52 @@ def log_prob(Y, B, d, G, beta, beta_tausq, dt):
                    beta_tausq[:, np.newaxis] * beta @ Omega)
     dlogL_beta_tausq = 0.5 * (P * beta_tausq ** (-1) - np.diag(BetaOmegaBeta))
 
-    return -log_likelihood, -dLogL_dd, -dlogL_dG, -dlogL_dbeta, -dlogL_beta_tausq
+    result = {
+        "dLogL_dd": -dLogL_dd,
+        "dlogL_dG": -dlogL_dG,
+        "dlogL_dbeta": -dlogL_dbeta,
+        "dlogL_beta_tausq": -dlogL_beta_tausq,
+        "loss": loss,
+        "log_likelihood": log_likelihood,
+        "penalty": log_prior_beta
+    }
+
+    return result
+
+
+def log_obj(Y, B, d, G, beta, beta_tausq, dt):
+    L, P = beta.shape
+    J = np.ones_like(Y)
+    diagdJ_plus_GBetaB = d[:, np.newaxis] * J + G @ beta @ B
+    lambda_del_t = np.exp(diagdJ_plus_GBetaB) * dt
+    Omega = create_first_diff_matrix(P)
+    Omega = Omega.T @ Omega
+    BetaOmegaBeta = beta @ Omega @ beta.T
+
+    log_likelihood = np.sum(diagdJ_plus_GBetaB * Y - lambda_del_t)
+    penalty = - beta_tausq.T @ np.diag(BetaOmegaBeta)
+    loss = log_likelihood + penalty
+
+    # Manual gradients
+    y_minus_lambdadt = Y - lambda_del_t
+    y_minus_lambdadt_times_B = y_minus_lambdadt @ B.T
+
+    dLogL_dd = np.sum(y_minus_lambdadt, axis=1)
+    dlogL_dG = y_minus_lambdadt_times_B @ beta.T
+    dlogL_dbeta = (G.T @ y_minus_lambdadt_times_B -
+                   2 * beta_tausq[:, np.newaxis] * beta @ Omega)
+
+    result = {
+        "dLogL_dd": -dLogL_dd,
+        "dlogL_dG": -dlogL_dG,
+        "dlogL_dbeta": -dlogL_dbeta,
+        "dlogL_beta_tausq": 0,
+        "loss": loss,
+        "log_likelihood": log_likelihood,
+        "penalty": penalty
+    }
+
+    return result
 
 
 def create_precision_matrix(P):
@@ -41,6 +86,27 @@ def create_precision_matrix(P):
     return Omega
 
 
+def create_first_diff_matrix(P):
+    D = np.zeros((P-1, P))
+    # fill the main diagonal with -1s
+    np.fill_diagonal(D, -1)
+    # fill the superdiagonal with 1s
+    np.fill_diagonal(D[:, 1:], 1)
+    return D
+
+
+def create_second_diff_matrix(P):
+    D = np.zeros((P-2, P))
+    # fill the main diagonal with 1s
+    np.fill_diagonal(D, 1)
+    # fill the subdiagonal and superdiagonal with -2s
+    np.fill_diagonal(D[:, 2:], 1)
+    np.fill_diagonal(D[:, 1:], -2)
+    # set the last element to 1
+    D[-1, -1] = 1
+    return D
+
+
 def compute_lambda(B, d, G, beta):
     J = np.ones((len(d), B.shape[1]))
     diagdJ_plus_GBetaB = d[:, np.newaxis] * J + np.dot(np.dot(G, beta), B)
@@ -53,41 +119,46 @@ def compute_latent_factors(B, beta):
     return N
 
 
-def compute_numerical_grad(Y, B, d, G, beta, beta_tausq, dt, eps=1e-6):
+def compute_numerical_grad(Y, B, d, G, beta, beta_tausq, dt, obj_func, eps=1e-6):
     """Compute numerical gradient of func w.r.t G"""
     G_grad = np.zeros_like(G)
     beta_grad = np.zeros_like(beta)
     d_grad = np.zeros_like(d)
     beta_tausq_grad = np.zeros_like(beta_tausq)
-    lk1, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+    result = obj_func(Y, B, d, G, beta, beta_tausq, dt)
+    lk1 = result["loss"]
     for i in range(G.shape[0]):
         for j in range(G.shape[1]):
             orig = G[i, j]
             G[i, j] = orig + eps
-            lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+            result = obj_func(Y, B, d, G, beta, beta_tausq, dt)
+            lk2 = result["loss"]
             G[i, j] = orig
-            G_grad[i, j] = (lk1 - lk2) / eps
+            G_grad[i, j] = (lk2 - lk1) / eps
 
     for i in range(beta.shape[0]):
         for j in range(beta.shape[1]):
             orig = beta[i, j]
             beta[i, j] = orig + eps
-            lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+            result = obj_func(Y, B, d, G, beta, beta_tausq, dt)
+            lk2 = result["loss"]
             beta[i, j] = orig
-            beta_grad[i, j] = (lk1 - lk2) / eps
+            beta_grad[i, j] = (lk2 - lk1) / eps
 
     for i in range(d.shape[0]):
         orig = d[i]
         d[i] = orig + eps
-        lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+        result = obj_func(Y, B, d, G, beta, beta_tausq, dt)
+        lk2 = result["loss"]
         d[i] = orig
-        d_grad[i] = (lk1 - lk2) / eps
+        d_grad[i] = (lk2 - lk1) / eps
 
     for i in range(beta_tausq.shape[0]):
         orig = beta_tausq[i]
         beta_tausq[i] = orig + eps
-        lk2, _, _, _, _ = log_prob(Y, B, d, G, beta, beta_tausq, dt)
+        result = obj_func(Y, B, d, G, beta, beta_tausq, dt)
+        lk2 = result["loss"]
         beta_tausq[i] = orig
-        beta_tausq_grad[i] = (lk1 - lk2) / eps
+        beta_tausq_grad[i] = (lk2 - lk1) / eps
 
     return -d_grad, -G_grad, -beta_grad, -beta_tausq_grad
