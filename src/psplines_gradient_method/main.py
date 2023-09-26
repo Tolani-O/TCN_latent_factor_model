b63a70d5100b7bacdc2106dd9d97338d5bd0416f
Sir Chester <tolani0017@gmail.com>
2023-09-26 11:22:55 -0400

    enable for new structure

--- src/psplines_gradient_method/main.py
+++ src/psplines_gradient_method/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from src.psplines_gradient_method.generate_bsplines import generate_bspline_matrix
 from src.simulate_data import DataAnalyzer
 from src.psplines_gradient_method.SpikeTrainModel import SpikeTrainModel
 from src.psplines_gradient_method.general_functions import compute_lambda, plot_binned, plot_spikes
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 self = DataAnalyzer().initialize()
 binned, time = self.sample_data()
 binned_spikes = np.where(binned >= 1)
-plot_spikes(binned_spikes)
+# plot_spikes(binned_spikes)
 intensity, latent_factors = self.intensity, self.latent_factors
 # plot_binned(binned)
 # self.plot_intensity_and_latents()
@@ -48,13 +49,13 @@ for epoch in range(num_epochs):
 
     result = model.log_obj_with_backtracking_line_search_and_time_warping(tau_psi, tau_beta, tau_G)
     loss = result["loss"]
-    # psi_penalty = result["psi_penalty"]
-    # beta_penalty = result["beta_penalty"]
-    # G_penalty = result["G_penalty"]
-    # dpsi = result["dlogL_dpsi"]
-    # dbeta = result["dlogL_dbeta"]
-    # dG_star = result["dlogL_dG"]
-    # dd = result["dLogL_dd"]
+    psi_penalty = result["psi_penalty"]
+    beta_penalty = result["beta_penalty"]
+    G_penalty = result["G_penalty"]
+    dpsi = result["dlogL_dpsi"]
+    dbeta = result["dlogL_dbeta"]
+    dG_star = result["dlogL_dG"]
+    dd = result["dlogL_dd"]
 
     losses.append(loss)
 
@@ -76,70 +77,69 @@ for epoch in range(num_epochs):
     if epoch % 100 == 0:
         print(f"Epoch {epoch}, Loss {loss}")
 
-num_epochs = len(losses)
-losses = np.array(losses)
-G_smooths = np.array(G_smooths)
-beta_smooths = np.array(beta_smooths)
-d_smooths = np.array(d_smooths)
-smooth_G = np.array(smooth_G)
-smooth_beta = np.array(smooth_beta)
-smooth_d = np.array(smooth_d)
-iters_d = np.array(iters_d)
-iters_G = np.array(iters_G)
-iters_beta = np.array(iters_beta)
-plt.plot(np.arange(0, num_epochs), losses[0:])
-plt.title('Losses')
-plt.show()
-plt.plot(np.arange(1, num_epochs), G_smooths)
-plt.title('G Smooths Numeric')
-plt.show()
-plt.plot(np.arange(1, num_epochs), beta_smooths)
-plt.title('Beta Smooths Numeric')
-plt.show()
-plt.plot(np.arange(1, num_epochs), d_smooths)
-plt.title('d Smooths Numeric')
-plt.show()
-plt.plot(np.arange(0, num_epochs), 1 / smooth_G)
-plt.title('G Smooths Line Search')
-plt.show()
-plt.plot(np.arange(0, num_epochs), 1 / smooth_beta)
-plt.title('Beta Smooths  Line Search')
-plt.show()
-plt.plot(np.arange(0, num_epochs), 1 / smooth_d)
-plt.title('d Smooths  Line Search')
-plt.show()
-plt.plot(np.arange(0, num_epochs), iters_G)
-plt.title('G Iters')
-plt.show()
-plt.plot(np.arange(0, num_epochs), iters_beta)
-plt.title('Beta Iters')
-plt.show()
-plt.plot(np.arange(0, num_epochs), iters_d)
-plt.title('d Iters')
-plt.show()
+# num_epochs = len(losses)
+# losses = np.array(losses)
+# G_smooths = np.array(G_smooths)
+# beta_smooths = np.array(beta_smooths)
+# d_smooths = np.array(d_smooths)
+# smooth_G = np.array(smooth_G)
+# smooth_beta = np.array(smooth_beta)
+# smooth_d = np.array(smooth_d)
+# iters_d = np.array(iters_d)
+# iters_G = np.array(iters_G)
+# iters_beta = np.array(iters_beta)
+# plt.plot(np.arange(0, num_epochs), losses[0:])
+# plt.title('Losses')
+# plt.show()
+# plt.plot(np.arange(1, num_epochs), G_smooths)
+# plt.title('G Smooths Numeric')
+# plt.show()
+# plt.plot(np.arange(1, num_epochs), beta_smooths)
+# plt.title('Beta Smooths Numeric')
+# plt.show()
+# plt.plot(np.arange(1, num_epochs), d_smooths)
+# plt.title('d Smooths Numeric')
+# plt.show()
+# plt.plot(np.arange(0, num_epochs), 1 / smooth_G)
+# plt.title('G Smooths Line Search')
+# plt.show()
+# plt.plot(np.arange(0, num_epochs), 1 / smooth_beta)
+# plt.title('Beta Smooths  Line Search')
+# plt.show()
+# plt.plot(np.arange(0, num_epochs), 1 / smooth_d)
+# plt.title('d Smooths  Line Search')
+# plt.show()
+# plt.plot(np.arange(0, num_epochs), iters_G)
+# plt.title('G Iters')
+# plt.show()
+# plt.plot(np.arange(0, num_epochs), iters_beta)
+# plt.title('Beta Iters')
+# plt.show()
+# plt.plot(np.arange(0, num_epochs), iters_d)
+# plt.title('d Iters')
+# plt.show()
 
-combined = np.concatenate(
-    [losses[:, np.newaxis], smooth_G[:, np.newaxis], smooth_beta[:, np.newaxis], smooth_d[:, np.newaxis],
-     iters_G[:, np.newaxis], iters_beta[:, np.newaxis], iters_d[:, np.newaxis]], axis=1)
-d_loss_increase = np.array(d_loss_increase)[:, np.newaxis]
-G_loss_increase = np.array(G_loss_increase)[:, np.newaxis]
-
-time_matrix = psi @ V
-B_psi = generate_bspline_matrix(B_func_n, time_matrix)
-lambda_manual = compute_lambda(B_psi, d, G_star, beta)
+time_matrix = model.psi @ model.V
+B_psi = generate_bspline_matrix(model.B_func_n, time_matrix)
+lambda_manual = compute_lambda(B_psi, model.d, model.G_star, model.beta)
 avg_lambda_manual = np.mean(lambda_manual, axis=0)
 plt.plot(time, avg_lambda_manual)
 plt.show()
 np.mean(np.square(intensity - lambda_manual))
-for i in range(K):
-    plt.plot(time, lambda_manual[i, :] + i * 0.1)
+for i in range(model.Y.shape[0]):
+    plt.plot(time, lambda_manual[9, :] + i * 2)
 plt.show()
 
-latent_factors_manual = beta @ V
+latent_factors_manual = model.beta @ model.V
 for i in range(L):
     # plt.plot(np.concatenate([[time[0] - dt], time]), beta[i, :])
     plt.plot(time, latent_factors_manual[i, :])
     plt.title(f'Factor [{i}, :]')
+plt.show()
+
+time_warping_matrix = model.psi @ model.V
+for i in range(model.Y.shape[0]):
+    plt.plot(time, time_warping_matrix[8, :]+ i * 0.01)
 plt.show()
 
 G_and_d = np.concatenate([G, d[:, np.newaxis]], axis=1)
