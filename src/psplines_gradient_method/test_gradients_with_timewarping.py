a10ba1d6242dc1bd9ac602aa52a64369549b0bf1
Sir Chester <tolani0017@gmail.com>
2023-10-31 22:30:56 -0400

    Forgot to update time warping test code on this branch

--- src/psplines_gradient_method/test_gradients_with_timewarping.py
+++ src/psplines_gradient_method/test_gradients_with_timewarping.py
@@ -3,7 +3,7 @@ from src.simulate_data import DataAnalyzer
 from src.psplines_gradient_method.SpikeTrainModel import SpikeTrainModel
 
 def main_function():
-    self = DataAnalyzer().initialize(K=100, R=3, max_offset=0)
+    self = DataAnalyzer().initialize(K=100, R=1, max_offset=0)
     binned, stim_time = self.sample_data()
 
     Y = binned  # K x T
@@ -15,19 +15,21 @@ def main_function():
     tau_psi = 1
     tau_beta = 1
 
-    dgamma, dalpha, dzeta, dchi, dd = model.compute_analytical_grad_time_warping(tau_psi, tau_beta)
-    dgamma_num, dalpha_num, dzeta_num, dchi_num, dd_num = model.compute_numerical_grad_time_warping_parallel(tau_psi, tau_beta)
+    dgamma, dalpha, dzeta, dchi = model.compute_analytical_grad_time_warping(tau_psi, tau_beta)
+    dgamma_num, dalpha_num, dzeta_num, dchi_num = model.compute_numerical_grad_time_warping_parallel(tau_psi, tau_beta)
     # account for constraints in dalpha and dzeta:
     dalpha[:, 1] = 0
     dzeta[:, 1] = 0
+    dgamma[(L - 1), :] = 0
+    dchi[:, 0] = 0
+
 
     dgamma_error = np.max(np.abs(dgamma - dgamma_num))
     dalpha_error = np.max(np.abs(dalpha - dalpha_num))
     dzeta_error = np.max(np.abs(dzeta - dzeta_num))
     dchi_error = np.max(np.abs(dchi - dchi_num))
-    dd_error = np.max(np.abs(dd - dd_num))
 
-    print(f"Max Errors: dgamma {dgamma_error}, dalpha {dalpha_error}, dzeta {dzeta_error}, dchi {dchi_error}, dd {dd_error}")
+    print(f"Max Errors: dgamma {dgamma_error}, dalpha {dalpha_error}, dzeta {dzeta_error}, dchi {dchi_error}")
 
 if __name__ == '__main__':
     main_function()
