fixed gradient testing code
gradient testing code bug fix

minor bug fixes

Big fix

src/psplines_gradient_method/SpikeTrainModel.py
@@ -71,7 +71,9 @@ class SpikeTrainModel:
         self.alpha = np.zeros((K, Q))
         self.zeta = np.zeros((R, Q))
         self.d1 = np.zeros((L, 1))
+        self.d1[0, :] = 0
         self.d2 = np.zeros((L, 1))
+        self.d2[0, :] = 0
 
         return self
 
@@ -166,8 +168,8 @@ class SpikeTrainModel:
                 d1_plus = self.d1 + learning_rate * dlogL_dd1
 
                 # set up variables to compute loss
+                d1_plus[0, :] = 0
                 s1 = np.exp(d1_plus)
-                s1[0, :] = 1
                 s1_norm = (1 / np.sum(s1)) * s1
                 beta_s1_penalty = - tau_beta * np.exp(2 * max_gamma) * (s1_norm.T @ diagBetaDeltaBeta).squeeze()
                 s1_norm_minus_linv = s1_norm - 1 / L
@@ -193,7 +195,6 @@ class SpikeTrainModel:
 
             # set up variables to compute loss in next round
             s1 = np.exp(self.d1)
-            s1[0, :] = 1
             s1_norm = (1 / np.sum(s1)) * s1
             beta_s1_penalty = - tau_beta * np.exp(2 * max_gamma) * (s1_norm.T @ diagBetaDeltaBeta).squeeze()
             s1_norm_minus_linv = s1_norm - 1 / L
@@ -209,8 +210,8 @@ class SpikeTrainModel:
                 d2_plus = self.d2 + learning_rate * dlogL_dd2
 
                 # set up variables to compute loss
+                d2_plus[0, :] = 0
                 s2 = np.exp(d2_plus)
-                s2[0, :] = 1
                 s2_norm = (1 / np.sum(s2)) * s2
                 beta_s2_penalty = - tau_beta * np.exp(2 * max_gamma) * (s2_norm.T @ diagBetaDeltaBeta).squeeze()
                 s2_norm_minus_linv = s2_norm - 1 / L
@@ -236,7 +237,6 @@ class SpikeTrainModel:
 
             # set up variables to compute loss in next round
             s2 = np.exp(self.d2)
-            s2[0, :] = 1
             s2_norm = (1 / np.sum(s2)) * s2
             beta_s2_penalty = - tau_beta * np.exp(2 * max_gamma) * (s2_norm.T @ diagBetaDeltaBeta).squeeze()
             s2_norm_minus_linv = s2_norm - 1 / L
@@ -261,14 +261,13 @@ class SpikeTrainModel:
             E = np.exp(self.c)  # variable
             E_beta_Bpsi = [E[k][:, np.newaxis] * beta @ b for k, b in enumerate(B_sparse)]
             GEBetaBPsi = [G[k][np.newaxis, :] @ e for k, e in enumerate(E_beta_Bpsi)]
-            dlogL_dchi = G * np.vstack(
-                [np.sum((1/GEBetaBPsi[k] * E_beta_Bpsi[k] - 1) * self.Y[k] - (np.eye(L) - G[k]) @ E_beta_Bpsi[k] * self.dt, axis=1)
+            dlogL_dchi = G * np.vstack([np.sum((1/GEBetaBPsi[k] * E_beta_Bpsi[k] - 1) * self.Y[k] - (np.eye(L) - G[k]) @ E_beta_Bpsi[k] * self.dt, axis=1)
                  for k in range(K)])
             while ct < max_iters:
                 chi_plus = self.chi + learning_rate * dlogL_dchi
 
                 # set up variables to compute loss
-                chi_plus[0, :] = 0
+                chi_plus[:, 0] = 0
                 exp_chi = np.exp(chi_plus)  # variable
                 G = (1 / np.sum(exp_chi, axis=1).reshape(-1, 1)) * exp_chi  # variable
                 GEBetaBPsi = np.vstack([G[k] @ e for k, e in enumerate(E_beta_Bpsi)])
@@ -295,8 +294,9 @@ class SpikeTrainModel:
             # smooth_c
             ct = 0
             learning_rate = 1
-            dlogL_dc = np.exp(self.chi + self.c - 2*np.vstack(maxes)) * np.vstack([((1/sum_exps_chi_plus_gamma_B[k] * self.Y[k] -
-                       1/sum_exps_chi[k] * self.dt) @ b.transpose()) @ np.exp(self.gamma - maxes[k]).T for k, b in enumerate(B_sparse)])
+            betaB = [beta @ b for k, b in enumerate(B_sparse)]
+            E_G = E * G
+            dlogL_dc = E_G * np.vstack([(1/(E_G[k][np.newaxis, :] @ betaB[k]) * self.Y[k] - self.dt) @ betaB[k].T for k, b in enumerate(B_sparse)])
             while ct < max_iters:
                 c_plus = self.c + learning_rate * dlogL_dc
 
@@ -526,6 +526,9 @@ class SpikeTrainModel:
     def compute_loss_objects(self, tau_psi, tau_beta, tau_s, time_warping):
         K, L = self.chi.shape
         R, Q = self.zeta.shape
+        self.chi[:, 0] = 0
+        self.d1[0, :] = 0
+        self.d2[0, :] = 0
         exp_alpha_c = (np.exp(self.alpha) @ self.alpha_prime_multiply) + np.repeat(self.alpha_prime_add, K, axis=0)
         psi = exp_alpha_c @ self.U_ones  # variable
         psi_norm = (1 / (psi[:, (Q - 1), np.newaxis])) * psi  # variable, called \psi' in the document
@@ -547,7 +550,6 @@ class SpikeTrainModel:
             kappa_penalty = 0
 
         s1 = np.exp(self.d1)
-        s1[0, :] = 1
         s1_norm = (1 / np.sum(s1)) * s1
         max_gamma = np.max(self.gamma)
         beta_minus_max = np.exp(self.gamma - max_gamma)
@@ -556,7 +558,6 @@ class SpikeTrainModel:
         s1_norm_minus_linv = s1_norm - 1 / L
         s1_penalty = - tau_s * (s1_norm_minus_linv.T @ s1_norm_minus_linv).squeeze()
         s2 = np.exp(self.d2)
-        s2[0, :] = 1
         s2_norm = (1 / np.sum(s2)) * s2
         beta_s2_penalty = - tau_beta * np.exp(2*max_gamma) * (s2_norm.T @ np.sum((beta_minus_max @ self.BDelta2TDelta2BT) * beta_minus_max, axis=1)).squeeze()
         s2_norm_minus_linv = s2_norm - 1 / L
@@ -593,79 +594,81 @@ class SpikeTrainModel:
 
         # define parameters
         K, L = self.chi.shape
-        R, Q = self.zeta.shape
         T = self.time.shape[0]
 
         # set up variables to compute loss
         objects = self.compute_loss_objects(tau_psi, tau_beta, tau_s, time_warping)
         B_sparse = objects["B_sparse"]
-        G = objects["G"]
-        GBeta = objects["GBeta"]
-        beta = objects["beta"]
         s1_norm = objects["s1_norm"]
         s2_norm = objects["s2_norm"]
-        exp_alpha_c = objects["exp_alpha_c"]
-        exp_zeta_c = objects["exp_zeta_c"]
-        kappa_norm = objects["kappa_norm"]
-        psi_norm = objects["psi_norm"]
-        time_matrix = objects["time_matrix"]
-        lambda_del_t = objects["lambda_del_t"]
-        GBetaBPsi = objects["GBetaBPsi"]
-        eps = objects["eps"]
+        maxes = objects["maxes"]
+        sum_exps_chi = objects["sum_exps_chi"]
+        sum_exps_chi_plus_gamma_B = objects["sum_exps_chi_plus_gamma_B"]
+        max_gamma = objects["max_gamma"]
+        beta_minus_max = objects["beta_minus_max"]
 
         # compute gradients
-        lambdainv_y_minus_del_t = 1 / GBetaBPsi * self.Y - self.dt
-        exp_KprimeBetaBDeltaT = np.exp(kprime * beta @ self.Delta2BT.T)
-        likelihood_component = G.T @ np.vstack([lambdainv_y_minus_del_t[k] @ b.transpose() for k, b in enumerate(B_sparse)])
-        s1_component = 2 * s1_norm * beta @ self.BDelta1TDelta1BT
-        s2_component = s2_norm * (2 / np.log(1 + eps + exp_KprimeBetaBDeltaT) * exp_KprimeBetaBDeltaT - 1) @ self.Delta2BT
-        dlogL_dgamma = beta * (likelihood_component - tau_beta * (s1_component + s2_component))
+        exp_chi = np.vstack([np.exp(self.chi[k] + self.c[k] - maxes[k]) for k in range(K)])  # variable
+        likelihood_component = exp_chi.T @ np.vstack(
+            [(1 / (sum_exps_chi_plus_gamma_B[k]) * self.Y[k] - 1 / sum_exps_chi[k] * self.dt) @ b.transpose() for k, b
+             in enumerate(B_sparse)])
+        s1_component = s1_norm * beta_minus_max @ self.BDelta1TDelta1BT
+        s2_component = s2_norm * beta_minus_max @ self.BDelta2TDelta2BT
+        dlogL_dgamma = beta_minus_max * np.exp(max_gamma) * (likelihood_component - 2 * tau_beta * np.exp(max_gamma) * (s1_component + s2_component))
 
-        dlogL_dd1 = s1_norm * (s1_norm.T - np.eye(L)) @ (
-                tau_beta * np.sum((beta @ self.BDelta1TDelta1BT) * beta, axis=1)[:, np.newaxis] +
-                2 * tau_s * (s1_norm - 1 / L))
+        diagBetaDeltaBeta = np.sum((beta_minus_max @ self.BDelta1TDelta1BT) * beta_minus_max, axis=1)[:, np.newaxis]
+        dlogL_dd1 = s1_norm * (s1_norm.T - np.eye(L)) @ (tau_beta * np.exp(2 * max_gamma) * diagBetaDeltaBeta +
+                                                         2 * tau_s * (s1_norm - 1 / L))
 
-        KprimeBetaBDelta2T = kprime * beta @ self.Delta2BT.T
-        dlogL_dd2 = 2 * s2_norm * (s2_norm.T - np.eye(L)) @ (tau_beta / kprime * np.sum(np.log(1 + eps + np.exp(
-                    KprimeBetaBDelta2T)) - 1 / 2 * KprimeBetaBDelta2T - np.log(2), axis=1)[:, np.newaxis] + tau_s * (s2_norm - 1 / L))
+        diagBetaDeltaBeta = np.sum((beta_minus_max @ self.BDelta2TDelta2BT) * beta_minus_max, axis=1)[:, np.newaxis]
+        dlogL_dd2 = s2_norm * (s2_norm.T - np.eye(L)) @ (tau_beta * np.exp(2 * max_gamma) * diagBetaDeltaBeta +
+                                                         2 * tau_s * (s2_norm - 1 / L))
 
         if time_warping:
-            y_minus_lambda_del_t = self.Y - lambda_del_t
-            knots_Bpsinminus1_1 = [
-                (self.knots_1 * BSpline.design_matrix(time, self.knots[:-1], (self.degree - 1)).transpose()).tocsc() for
-                time in time_matrix]
-            knots_Bpsinminus1_2 = [vstack([b_deriv[1:], csr_array((1, R * T))]).tocsc() for b_deriv in
-                                   knots_Bpsinminus1_1]
-            GBetaBPsiDeriv = np.vstack([GBeta[k] @ (knots_Bpsinminus1_1[k] - knots_Bpsinminus1_2[k]) for k in range(K)])
-            GBetaBPsiDerivXyLambda = GBetaBPsiDeriv * y_minus_lambda_del_t
-            psi_norm_Omega = psi_norm @ self.Omega_psi_B
-            kappa_norm_Omega = kappa_norm @ self.Omega_psi_B
-            dlogL_dalpha = psi_norm[:, 1, np.newaxis] * exp_alpha_c * np.vstack(
-                [0.5 * max(self.time) * self.degree * np.sum(
-                    GBetaBPsiDerivXyLambda * ((self.U_ones[q] - psi_norm) @ hstack([self.V] * R)), axis=1) -
-                 2 * tau_psi * np.sum(psi_norm_Omega * (self.U_ones[q] - psi_norm), axis=1)
-                 for q in range(Q)]).T
-
-            dlogL_dzeta = kappa_norm[:, 1, np.newaxis] * exp_zeta_c * np.vstack([0.5 * max(self.time) * self.degree *
-                                                                                 np.sum(np.sum(
-                                                                                     GBetaBPsiDerivXyLambda * ((
-                                                                                                                       self.U_ones[
-                                                                                                                           q] - kappa_norm) @ self.V).flatten(),
-                                                                                     axis=0).reshape((R, T)), axis=1) -
-                                                                                 2 * tau_psi * np.sum(
-                kappa_norm_Omega * (self.U_ones[q] - kappa_norm), axis=1)
-                                                                                 for q in range(Q)]).T
+            dlogL_dalpha = 0
+            dlogL_dzeta = 0
+            # y_minus_lambda_del_t = self.Y - lambda_del_t
+            # knots_Bpsinminus1_1 = [
+            #     (self.knots_1 * BSpline.design_matrix(time, self.knots[:-1], (self.degree - 1)).transpose()).tocsc() for
+            #     time in time_matrix]
+            # knots_Bpsinminus1_2 = [vstack([b_deriv[1:], csr_array((1, R * T))]).tocsc() for b_deriv in
+            #                        knots_Bpsinminus1_1]
+            # GBetaBPsiDeriv = np.vstack([GBeta[k] @ (knots_Bpsinminus1_1[k] - knots_Bpsinminus1_2[k]) for k in range(K)])
+            # GBetaBPsiDerivXyLambda = GBetaBPsiDeriv * y_minus_lambda_del_t
+            # psi_norm_Omega = psi_norm @ self.Omega_psi_B
+            # kappa_norm_Omega = kappa_norm @ self.Omega_psi_B
+            # dlogL_dalpha = psi_norm[:, 1, np.newaxis] * exp_alpha_c * np.vstack(
+            #     [0.5 * max(self.time) * self.degree * np.sum(
+            #         GBetaBPsiDerivXyLambda * ((self.U_ones[q] - psi_norm) @ hstack([self.V] * R)), axis=1) -
+            #      2 * tau_psi * np.sum(psi_norm_Omega * (self.U_ones[q] - psi_norm), axis=1)
+            #      for q in range(Q)]).T
+            #
+            # dlogL_dzeta = kappa_norm[:, 1, np.newaxis] * exp_zeta_c * np.vstack([0.5 * max(self.time) * self.degree *
+            #                                                                      np.sum(np.sum(
+            #                                                                          GBetaBPsiDerivXyLambda * ((
+            #                                                                                                            self.U_ones[
+            #                                                                                                                q] - kappa_norm) @ self.V).flatten(),
+            #                                                                          axis=0).reshape((R, T)), axis=1) -
+            #                                                                      2 * tau_psi * np.sum(
+            #     kappa_norm_Omega * (self.U_ones[q] - kappa_norm), axis=1)
+            #                                                                      for q in range(Q)]).T
         else:
             dlogL_dalpha = 0
             dlogL_dzeta = 0
 
-        lambdainv = 1 / (GBetaBPsi)
-        beta_Bpsi = [beta @ b for k, b in enumerate(B_sparse)]
-        dlogL_dchi = G * np.vstack(
-            [np.sum((lambdainv[k] * beta_Bpsi[k] - 1) * self.Y[k] - (np.eye(L) - G[k]) @ beta_Bpsi[k] * self.dt, axis=1)
-             for k in range(K)])
+        beta = np.exp(self.gamma)  # variable
+        exp_chi = np.exp(self.chi)  # variable
+        G = (1 / np.sum(exp_chi, axis=1).reshape(-1, 1)) * exp_chi  # variable
+        E = np.exp(self.c)  # variable
+        E_beta_Bpsi = [E[k][:, np.newaxis] * beta @ b for k, b in enumerate(B_sparse)]
+        GEBetaBPsi = [G[k][np.newaxis, :] @ e for k, e in enumerate(E_beta_Bpsi)]
+        dlogL_dchi = G * np.vstack([np.sum((1 / GEBetaBPsi[k] * E_beta_Bpsi[k] - 1) * self.Y[k] - (np.eye(L) - G[k]) @ E_beta_Bpsi[k] * self.dt, axis=1) for k in range(K)])
 
-        return dlogL_dgamma, dlogL_dd1, dlogL_dd2, dlogL_dalpha, dlogL_dzeta, dlogL_dchi
+        betaB = [beta @ b for k, b in enumerate(B_sparse)]
+        E_G = E * G
+        dlogL_dc = E_G * np.vstack([(1 / (E_G[k][np.newaxis, :] @ betaB[k]) * self.Y[k] - self.dt) @ betaB[k].T for k, b in enumerate(B_sparse)])
+
+        return dlogL_dgamma, dlogL_dd1, dlogL_dd2, dlogL_dalpha, dlogL_dzeta, dlogL_dchi, dlogL_dc
 
     def compute_grad_chunk(self, name, variable, i, eps, tau_psi, tau_beta, tau_s, loss, time_warping=False):
 
@@ -693,53 +696,60 @@ class SpikeTrainModel:
         K, L = self.chi.shape
         R = self.trials
 
-        objects = self.compute_loss_objects(tau_psi, tau_beta, tau_s)
+        objects = self.compute_loss_objects(tau_psi, tau_beta, tau_s, time_warping)
         loss = objects['loss']
         pool = mp.Pool()
 
-        # alpha gradient
-        alpha_async_results = [
-            pool.apply_async(self.compute_grad_chunk, args=('alpha', self.alpha, k, eps, tau_psi, tau_beta, loss)) for k
-            in range(K)]
+        # gamma gradient
+        gamma_async_results = [
+            pool.apply_async(self.compute_grad_chunk, args=('gamma', self.gamma, l, eps, tau_psi, tau_beta, tau_s, loss))
+            for l in range(L)]
 
         # d1 gradient
         d1_async_results = [
-            pool.apply_async(self.compute_grad_chunk, args=('d1', self.d1, l, eps, tau_psi, tau_beta, loss)) for
+            pool.apply_async(self.compute_grad_chunk, args=('d1', self.d1, l, eps, tau_psi, tau_beta, tau_s, loss)) for
             l in range(L)]
 
         # d2 gradient
         d2_async_results = [
-            pool.apply_async(self.compute_grad_chunk, args=('d2', self.d2, l, eps, tau_psi, tau_beta, loss)) for
+            pool.apply_async(self.compute_grad_chunk, args=('d2', self.d2, l, eps, tau_psi, tau_beta, tau_s, loss)) for
             l in range(L)]
 
         if time_warping:
             # zeta gradient
             zeta_async_results = [
-                pool.apply_async(self.compute_grad_chunk, args=('zeta', self.zeta, r, eps, tau_psi, tau_beta, loss)) for
+                pool.apply_async(self.compute_grad_chunk, args=('zeta', self.zeta, r, eps, tau_psi, tau_beta, tau_s, loss)) for
                 r in range(R)]
 
-            # gamma gradient
-            gamma_async_results = [
-                pool.apply_async(self.compute_grad_chunk, args=('gamma', self.gamma, l, eps, tau_psi, tau_beta, loss))
-                for l in range(L)]
+            # alpha gradient
+            alpha_async_results = [
+                pool.apply_async(self.compute_grad_chunk, args=('alpha', self.alpha, k, eps, tau_psi, tau_beta, tau_s, loss))
+                for k
+                in range(K)]
 
         # chi gradient
         chi_async_results = [
-            pool.apply_async(self.compute_grad_chunk, args=('chi', self.chi, k, eps, tau_psi, tau_beta, loss)) for k in
+            pool.apply_async(self.compute_grad_chunk, args=('chi', self.chi, k, eps, tau_psi, tau_beta, tau_s, loss)) for k in
+            range(K)]
+
+        # c gradient
+        c_async_results = [
+            pool.apply_async(self.compute_grad_chunk, args=('c', self.c, k, eps, tau_psi, tau_beta, tau_s, loss)) for k in
             range(K)]
 
         pool.close()
         pool.join()
 
-        alpha_grad = np.vstack([r.get() for r in alpha_async_results])
+        gamma_grad = np.vstack([r.get() for r in gamma_async_results])
         d1_grad = np.vstack([r.get() for r in d1_async_results])
         d2_grad = np.vstack([r.get() for r in d2_async_results])
         if time_warping:
             zeta_grad = np.vstack([r.get() for r in zeta_async_results])
-            gamma_grad = np.vstack([r.get() for r in gamma_async_results])
+            alpha_grad = np.vstack([r.get() for r in alpha_async_results])
         else:
             zeta_grad = 0
-            gamma_grad = 0
+            alpha_grad = 0
         chi_grad = np.vstack([r.get() for r in chi_async_results])
+        c_grad = np.vstack([r.get() for r in c_async_results])
 
-        return gamma_grad, d1_grad, d2_grad, alpha_grad, zeta_grad, chi_grad
+        return gamma_grad, d1_grad, d2_grad, alpha_grad, zeta_grad, chi_grad, c_grad

src/psplines_gradient_method/test_gradients_with_timewarping.py
@@ -14,14 +14,16 @@ def main_function():
     # Training hyperparameters
     tau_psi = 1
     tau_beta = 1
+    tau_s = 1
 
-    dgamma, dd1, dd2, dalpha, dzeta, dchi = model.compute_analytical_grad_time_warping(tau_psi, tau_beta)
-    dgamma_num, dd1_num, dd2_num, dalpha_num, dzeta_num, dchi_num = model.compute_numerical_grad_time_warping_parallel(tau_psi, tau_beta)
+    dgamma, dd1, dd2, dalpha, dzeta, dchi, dc = model.compute_analytical_grad_time_warping(tau_psi, tau_beta, tau_s)
+    dgamma_num, dd1_num, dd2_num, dalpha_num, dzeta_num, dchi_num, dc_num = model.compute_numerical_grad_time_warping_parallel(tau_psi, tau_beta, tau_s)
     # account for constraints in dalpha and dzeta:
     dalpha[:, 1] = 0 # might need to fix dalpha[:, 0] = 0 as well
     dzeta[:, 1] = 0 # might need to fix dzeta[:, 0] = 0 as well
-    # dgamma[(L - 1), :] = 0 # no longee fixing constant latent factor
     dchi[:, 0] = 0
+    dd1[:, 0] = 0
+    dd2[:, 0] = 0
 
 
     dgamma_error = np.max(np.abs(dgamma - dgamma_num))
@@ -30,8 +32,9 @@ def main_function():
     dalpha_error = np.max(np.abs(dalpha - dalpha_num))
     dzeta_error = np.max(np.abs(dzeta - dzeta_num))
     dchi_error = np.max(np.abs(dchi - dchi_num))
+    dc_error = np.max(np.abs(dc - dc_num))
 
-    print(f"Max Errors: dgamma {dgamma_error}, dd1 {dd1_error}, dd2 {dd2_error}, dalpha {dalpha_error}, dzeta {dzeta_error}, dchi {dchi_error}")
+    print(f"Max Errors: dgamma {dgamma_error}, dd1 {dd1_error}, dd2 {dd2_error}, dalpha {dalpha_error}, dzeta {dzeta_error}, dchi {dchi_error}, dc {dc_error}")
 
 if __name__ == '__main__':
     main_function()